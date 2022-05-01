cc029654962742c9868ea0d47e0af41106204fd5
wasiushisir <wasiuahmed410@gmail.com>
2022-05-01 14:32:40 +0600

    create item

--- index.js
+++ index.js
@@ -62,6 +62,16 @@ async function run()
         })   
 
 
+        //delete 
+
+        app.delete('/items/:id',async(req,res)=>{
+            const id=req.params.id;
+            const query={_id:ObjectId(id)}
+            const result=await itemsCollection.deleteOne(query)
+            res.send(result) 
+        })
+
+
 
 
 
